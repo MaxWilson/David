c3f05e931fe31577669a14677247fdfd7db74c72
Max Wilson <wilson.max@gmail.com>
2019-09-10 20:20:39 -0700

    Attempt at fix

--- David/David.cpp
+++ David/David.cpp
@@ -16,6 +16,7 @@ enum ExpressionType { Variable, Unary, Binary };
 
 // would be cleaner to use std::variant here, see https://arne-mertz.de/2018/05/modern-c-features-stdvariant-and-stdvisit/ for examples
 // but I'm trying to avoid introducing new concepts for you to worry about
+// Example: 	auto expression = Expression(Expression(Expression("A"), AND, Expression("B")), IMPLIES, Expression(NOT, Expression("A")));
 struct Expression {
 	ExpressionType type;
 	shared_ptr<Expression> expression1;
@@ -38,6 +39,106 @@ typedef pair<Expression, string> ParseOutput;
 
 struct ParseFailure { };
 struct ImpossibleFailure { }; // Mark things that should never happen
+
+bool match(string input, string pattern) {
+	return (input.size() >= pattern.size()) && (input.substr(0, pattern.size()) == pattern);
+}
+string consumeWhitespace(string input) {
+	string output = input;;
+	while (output[0] == ' ') {
+		output = output.substr(1, output.size() - 1);
+	}
+	return output;
+}
+string consume(string pattern, string input) {
+	if (!match(input, pattern)) {
+		throw ParseFailure();
+	}
+	auto output = input.substr(pattern.size(), input.size() - pattern.size()); // get everything but the used-up part of input
+	return consumeWhitespace(output);
+}
+
+bool isLetter(char c) {
+	return ('a' <= c && c <= 'z') || ('A' <= c && c <= 'Z');
+}
+
+const int MaxDepth = 10; // horrible hack to get around problem with recursive-descent parser on this particular grammar
+							// I told you parsing was complicated! (And not worth worrying about for this assignment.)
+							// This parameter means that the parser will fail on "large" truth tables 
+							// but it will never go into an infinite loop
+
+ParseOutput ParseVariable(string input) {
+	input = consumeWhitespace(input);
+	auto variableName = string();
+	for (int i = 0; 0 < input.size(); ++i) {
+		if (isLetter(input[i])) {
+			variableName = variableName + input[i];
+		}
+		else break;
+	}
+	if (variableName.size() > 0) {
+		return pair<Expression, string>(Expression(variableName), consume(variableName, input));
+	}
+	else throw ParseFailure();
+}
+
+ParseOutput ParseExpression(string input, int recursiveDepth); // forward declaration so we can call it recursively
+
+ParseOutput ParseUnaryExpression(string input, int recursiveDepth) {
+	auto result = ParseExpression(consume("!", input), recursiveDepth + 1);
+	return pair<Expression, string>(Expression(NOT, result.first), result.second);
+}
+
+ParseOutput ParseBinaryExpression(string input, int recursiveDepth) {
+	auto lhs = ParseExpression(input, recursiveDepth + 1);
+	BinaryOperator op;
+	string remaining;
+	auto hadSuccess = false;
+	try {
+		remaining = consume("/\\", lhs.second); // Must escape \ in string, so /\ is written as "/\\"
+		op = AND;
+		hadSuccess = true;
+	} catch(ParseFailure) { }
+	if (!hadSuccess) {
+		try {
+			remaining = consume("\\/", lhs.second); // Must escape \ string, so \/ is written as "\\/"
+			op = OR;
+			hadSuccess = true;
+		}
+		catch (ParseFailure) {}
+	}
+	if (!hadSuccess) {
+		remaining = consume("->", lhs.second); // this time if it fails we don't catch it, we just give up
+		op = IMPLIES;
+		hadSuccess = true;
+	}
+	// at this point we haven't thrown a ParseFailure, so the parse is valid so far. We try to get a second operand.
+
+	auto rhs = ParseExpression(remaining, recursiveDepth+1);
+	return pair<Expression, string>(Expression(lhs.first, op, rhs.first), rhs.second);
+}
+
+ParseOutput ParseExpression(string input, int recursiveDepth) {
+	if (recursiveDepth > MaxDepth) throw ParseFailure();
+
+	try {
+		return ParseBinaryExpression(input, recursiveDepth + 1);
+	}
+	catch (ParseFailure) {}
+	// try again as a unary expression
+	try {
+		return ParseUnaryExpression(input, recursiveDepth + 1);
+	}
+	catch (ParseFailure) {}
+	// try just plain variable, and if that fails then give up
+	return ParseVariable(input);
+}
+
+// recursive-descent parser
+Expression Parse(string input) {
+	auto result = ParseExpression(input, 0);
+	return result.first;
+}
 
 string render(Expression expr) {
 	if (expr.type == Variable) return expr.variableName;
@@ -137,11 +238,9 @@ auto printTruthTable(Expression expr) {
 
 int main()
 {
-	auto expression = Expression(Expression(Expression("A"), AND, Expression("B")), IMPLIES, Expression(NOT, Expression("A")));
-	cout << render(expression) << endl;
-	auto variables = extractVariables(expression);
-	//for_each(begin(variables), end(variables), [](auto v) { cout << v << " ";  });
-	printTruthTable(expression);
+	auto expr = Parse("A /\\ B -> C"); // enter your own expression here
+	cout << render(expr) << endl;
+	printTruthTable(expr);
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
